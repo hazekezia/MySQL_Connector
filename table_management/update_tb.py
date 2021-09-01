535dce5e21d0d9c05cd9081a6eb2feb46b4ff668
Jonathan Sitohang <hazekezia@gmail.com>
2021-09-01 12:34:19 +0700

    Adding update_tb.py

---
+++ table_management/update_tb.py
@@ -0,0 +1,17 @@
+import mysql.connector
+import sys
+sys.path.insert(0, "../MySQL Python Connector")
+from connecting import connecting_db1
+
+db = connecting_db1()
+
+cursor = db.cursor()
+
+# UPDATE <nama tabel> SET <nama kolom=%s> WHERE <id = %s>
+SQLCommand = "UPDATE api_test SET name=%s, harga=%s WHERE id=%s"
+value = ("Alex", "29000", 1)
+
+cursor.execute(SQLCommand, value)
+db.commit()
+
+print("{} data diubah".format(cursor.rowcount))
