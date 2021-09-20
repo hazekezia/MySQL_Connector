cfe03b9881517e733bef87e2b2f3e7644c362e86
Jonathan Sitohang <hazekezia@gmail.com>
2021-09-20 18:20:15 +0700

    archive

--- table_management/table/tb_select_print.py
+++ table_management/table/tb_select_print.py
@@ -8,7 +8,7 @@ db = connecting_db1()
 cursor = db.cursor()
 
 #Select * FROM <nama table>
-SQLCommand = "SELECT * FROM one"
+SQLCommand = "SELECT * FROM two"
 cursor.execute(SQLCommand)
 
 # fetchall() -> ambil semua data
